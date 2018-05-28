30c725d40232c34726c5ed4fe8dad8e09349b96d
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2018-05-28 22:58:22 +0500

    Utility module game_functions has been created. Main module has been modified taking into account it

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,5 @@
-import sys
 import pygame
+import game_functions as gf
 from settings import Settings
 from ship import Ship
 
@@ -18,9 +18,7 @@ def run_game():
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         # Redraw the screen during each pass through the loop.
         screen.fill(settings.bg_color)
