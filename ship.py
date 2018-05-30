47b48829644ab12f651bf6662af23716fdeaa3b7
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2018-05-30 19:21:55 +0500

    Initial position of ship has been changed to center of left side.

--- ship.py
+++ ship.py
@@ -13,8 +13,8 @@ class Ship():
         self.screen_rect = screen.get_rect()
 
         # Start each new ship at the bottom center of the screen.
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.left = self.screen_rect.left
+        self.rect.centery = self.screen_rect.centery
 
         # Store a decimal value for the ship's center.
         self.center = float(self.rect.centerx)
