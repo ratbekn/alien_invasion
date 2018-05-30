a4b715761e25b4496adbb5ee8469a0860cea1555
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2018-05-30 18:32:32 +0500

    Handling of pressing the space has been added. Redrawing of bullets has been added.

--- game_functions.py
+++ game_functions.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
 def check_events(settings, screen, ship, bullets):
@@ -8,17 +9,21 @@ def check_events(settings, screen, ship, bullets):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, settings, screen, ship, bullets):
     """Respond to keypresses."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -33,6 +38,9 @@ def update_screen(settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(settings.bg_color)
+    # Redraw all bullets behind ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     # Make the most recently drawn screen visible.
