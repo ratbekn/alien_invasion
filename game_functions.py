bb415fbc4cdf78804e8594e6cdfd399b8b988e81
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2018-05-30 18:59:44 +0500

    Code has been refactored: created update_bullets and fire_bullet functions.

--- game_functions.py
+++ game_functions.py
@@ -21,10 +21,15 @@ def check_keydown_events(event, settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
-        if len(bullets) < settings.bullets_allowed:
-            new_bullet = Bullet(settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(settings, screen, ship, bullets)
+
+
+def fire_bullet(settings, screen, ship, bullets):
+    """Fire a bullet if limit not reached yet."""
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < settings.bullets_allowed:
+        new_bullet = Bullet(settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -46,3 +51,13 @@ def update_screen(settings, screen, ship, bullets):
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    # Update bullet positions.
+    bullets.update()
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
