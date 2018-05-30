b80f1fcd261c223bf9936025bcaf195f0ca64de5
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2018-05-30 19:30:28 +0500

    Ship's motion along x has been changed to a motion along y.

--- ship.py
+++ ship.py
@@ -18,7 +18,7 @@ class Ship():
         self.rect.centery = self.screen_rect.centery
 
         # Store a decimal value for the ship's center.
-        self.center = float(self.rect.centerx)
+        self.center = float(self.rect.centery)
 
         # Movement flags.
         self.moving_right = False
@@ -27,13 +27,13 @@ class Ship():
     def update(self):
         """Update the ship's position based on the movement flag."""
         # Update the ship's center value, not the rect.
-        if self.moving_right and self.rect.right < self.screen_rect.right:
+        if self.moving_right and self.rect.bottom < self.screen_rect.bottom:
             self.center += self.settings.ship_speed_factor
-        if self.moving_left and self.rect.left > 0:
+        if self.moving_left and self.rect.top > 0:
             self.center -= self.settings.ship_speed_factor
 
         # Update rect object from self.center
-        self.rect.centerx = self.center
+        self.rect.centery = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
