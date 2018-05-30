3f5d8b71f397b97f13b63b552541ccc902ff55c7
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2018-05-30 14:38:22 +0500

    Piloting the ship has been added

--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,21 @@ import sys
 import pygame
 
 
-def check_events():
+def check_events(ship):
     """Respond to keppresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(settings, screen, ship):
